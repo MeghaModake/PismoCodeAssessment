e56c09930beabb0dd33f85202cd430a88579898e
Megha Modake <Megha.Modake@ibm.com>
2025-09-09 17:49:11 +0100

    Fixed failing test cases

--- main_test.go
+++ main_test.go
@@ -9,11 +9,25 @@ import (
 	"os"
 	"pismo-code-assessment/datastruct"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
+var router *mux.Router
+var logger *log.Logger
+
+func setup() {
+	logger = log.New(os.Stdout, "TEST_log", log.LstdFlags)
+	router = NewRouter(logger)
+}
+func TestMain(m *testing.M) {
+
+	setup()
+	code := m.Run()
+	os.Exit(code)
+}
+
 func TestCreateAndGetAccount(t *testing.T) {
-	logger := log.New(os.Stdout, "TEST_log", log.LstdFlags)
-	router := NewRouter(logger)
 
 	// Create account
 	acc := datastruct.CreateAccountsRequest{Document_Number: "100"}
@@ -23,8 +37,8 @@ func TestCreateAndGetAccount(t *testing.T) {
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 
-	if w.Code != http.StatusOK {
-		t.Fatalf("expected 400, got %d", w.Code)
+	if w.Code != http.StatusCreated {
+		t.Fatalf("expected 201, got %d", w.Code)
 	}
 	var got datastruct.Account
 	if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
@@ -41,8 +55,8 @@ func TestCreateAndGetAccount(t *testing.T) {
 	w = httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 
-	if w.Code != http.StatusOK {
-		t.Fatalf("expected 200, got %d", w.Code)
+	if w.Code != http.StatusCreated {
+		t.Fatalf("expected 201, got %d", w.Code)
 	}
 
 	if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
@@ -51,8 +65,8 @@ func TestCreateAndGetAccount(t *testing.T) {
 	if got.Document_Number != "101" || got.Account_ID != 2 {
 		t.Errorf("unexpected account: %+v", got)
 	}
-	if w.Code != http.StatusOK {
-		t.Fatalf("expected 200, got %d", w.Code)
+	if w.Code != http.StatusCreated {
+		t.Fatalf("expected 201, got %d", w.Code)
 	}
 	// // Create another account with non unique Document_Number
 	acc = datastruct.CreateAccountsRequest{Document_Number: "101"}
@@ -107,19 +121,26 @@ func TestCreateAndGetAccount(t *testing.T) {
 }
 
 func TestCreateAndGetTransaction(t *testing.T) {
-	logger := log.New(os.Stdout, "TEST_log", log.LstdFlags)
-	router := NewRouter(logger)
 
-	tx := datastruct.CreateTransactionRequest{Account_ID: 1, OperationType_ID: 1, Amount: 50}
-	body, _ := json.Marshal(tx)
+	// Create account first
+	acc := datastruct.CreateAccountsRequest{Document_Number: "100"}
+	body, _ := json.Marshal(acc)
 
-	// Create transaction
-	req := httptest.NewRequest("POST", "/transactions", bytes.NewReader(body))
+	req := httptest.NewRequest("POST", "/accounts", bytes.NewReader(body))
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 
-	if w.Code != http.StatusOK {
-		t.Fatalf("expected 200, got %d", w.Code)
+	// create transaction
+	tx := datastruct.CreateTransactionRequest{Account_ID: 1, OperationType_ID: 1, Amount: 50}
+	body, _ = json.Marshal(tx)
+
+	// Create transaction
+	req = httptest.NewRequest("POST", "/transactions", bytes.NewReader(body))
+	w = httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusCreated {
+		t.Fatalf("expected 201, got %d", w.Code)
 	}
 	var got datastruct.Transaction
 	if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
